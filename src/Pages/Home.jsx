8801dfef7148498d7799c3246677f5453b61b36a
levijazz18 <aiyeolamathew@gmail.com>
2023-02-09 17:26:56 +0100

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div> <h1>I love my self</h1></div>;
+  return (
+    <div>
+      {" "}
+      <h1>I love my self</h1>
+      <p>i know right</p>
+    </div>
+  );
 };
 
 export default Home;
