541a52ec5191bceaaceb551f5af63d92d57b74aa
levijazz18 <aiyeolamathew@gmail.com>
2023-02-18 06:19:24 +0100

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      sm:'480px',
+      sm: '480px',
       md: '768px',
-      lg: '1440px',
-      xl: '1440px'
+      lg: '976px',
+      xl: '1440px',
     },
-    
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
-      // colors:(
-      //   red: 'hsl(12, 88%, 59%)',
-      // ),
-    },
-  },
-  plugins: [],
-};
- 
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
